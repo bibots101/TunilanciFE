786e1be3bea7aae883fbb9203f052d274ed281b7
Lourimi Wassim <wassimlourimi@gmail.com>
2025-01-30 18:08:23 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ import { Environment } from "./environment.interface";
 
 export const environment: Environment = {
     production: true,
-    API_URL: 'http://127.0.0.1:5000'
+    API_URL: 'https://tunilancibe.onrender.com'
 };
